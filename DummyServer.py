2dec464e41560dd1b0a3d4718c1d70b500b1f542
Daniel Karlsson <oumu@Daniels-MacBook-Pro.local>
2018-02-20 14:17:09 +0100

    Added files, modified Dymmyserver.py

--- DummyServer.py
+++ DummyServer.py
@@ -6,15 +6,13 @@ app = Flask(__name__)
 @app.route('/init', methods=['GET', 'POST'])
 def nodeInit():
    content = request.json
-   print content['SERVER_IP']
-   print content['SERVER_PORT']
-   print content['INIT_RAW']
-   print content['ASSIGNED_NODE_NAME']
    print content['NODE_IP']
    print content['NODE_PORT']
+   if content['NODE_ID'] == '': content['NODE_ID'] = 'nodeX'
+   print content['NODE_ID']
 
-   content['ASSIGNED_NODE_NAME'] = 'Node#1'
-   content['INIT_RAW'] = 0     
+
+   
    return jsonify(content)
    
 
