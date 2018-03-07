4748d1e7a4458e514ea279ad2fd6d7a41a7b46f3
Daniel Karlsson <oumu@Daniels-MacBook-Pro.local>
2018-03-07 13:28:57 +0100

    Added routes for new database queries

--- backend/restfulapi.py
+++ backend/restfulapi.py
@@ -11,6 +11,8 @@ app = Flask(__name__)
 '''
 Add function to check authentication(HMAC, pre-shared key etc.) 
 '''
+
+#Post/Get routes
 @app.route('/init', methods=['GET', 'POST'])
 def process_node_init():
     content = request.json
@@ -18,6 +20,7 @@ def process_node_init():
     return jsonify(init_packet)
 
 
+#Post routes
 @app.route('/Temp', methods=['POST'])
 def process_reading_post():
     content = request.json
@@ -25,15 +28,28 @@ def process_reading_post():
     return jsonify(node_data), 201
 
 
-@app.route('/GET/reading/<reading_type>')
+#Get routes
+@app.route('reading/<reading_type>', methods = ['GET'])
 def process_reading_get(reading_type):
     pass
 
-@app.route("/GET/sensors/<sensor_name>", methods = ['GET'])
+@app.route("/sensors/<sensor_name>", methods = ['GET'])
 def getSensors(sensor_name):
     Server.get_connected_sensors(sensor_name)
-    
 
+@app.route("/redings/<node_location>/", methods = ['GET'])
+def readings_by_location():
+    return json.dumps(Server.get_readings_by_location(node_location))
+
+@app.route("/readings/<reading_type>/<node_location>", methods = ['GET'])
+def readings_by_type_and_location(node_location, reading_type):
+    return json.dumps(Server.get_readings_by_location_and_type(node_location, reading_type))
+
+@app.route("/readings/<reading_type>/", methods = ['GET'])
+def readings_by_type(reading_type):
+    return json.dumps(Server.get_readings_by_type(reading_type))
+
+    
 def main():
     Server.init_database()
     app.run(debug = True, host = '0.0.0.0', port = 5000)
