d8c67617639d52b8a016a70bb278532e3a6e03ce
Jesper Eriksson <jespererik@users.noreply.github.com>
2018-04-04 14:22:31 +0200

    add class for location

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,54 +6,60 @@ export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { readings: [] , nodes: []};
+    this.state = { readings: [] , nodes: [], locations: []};
   }
 
   componentDidMount() {
-    this.fetchReadings();
-    this.fetchNodes();
-  }
-
-  fetchReadings(){
-    fetch('http://localhost:3000/api/nodes/NODE_1/sensors/DHT11/readings')
-    .then(response => response.json())
-    .then(jsonData =>{
-      console.log(jsonData)
-      const readings = jsonData.map(object => (object));
-      this.setState({readings})
-    })
-  }
-
-  fetchNodes(){
-    fetch('http://localhost:3000/api/nodes')
-    .then(response => response.json())
-    .then(jsonData =>{
-      console.log(jsonData)
-      const nodes = jsonData.nodes.map(object => (object)) //this is an object with an array in it
-      this.setState({nodes})
-    })
   }
 
   render() {
-    var listStyle = {
-      // width:  500,
-      // margin: 100,
-      fontFamily: "monospace",
-      fontSize: 12,
-      backgroundColor: "#FFF",
-      WebkitFilter: "drop-shadow(0px 0px 5px #666)",
-      filter: "drop-shadow(0px 0px 5px #666)",
-    };
+    // var listStyle = {
+    //   // width:  500,
+    //   // margin: 100,
+    //   fontFamily: "monospace",
+    //   fontSize: 12,
+    //   backgroundColor: "#FFF",
+    //   WebkitFilter: "drop-shadow(0px 0px 5px #666)",
+    //   filter: "drop-shadow(0px 0px 5px #666)",
+    //};
     return (
-      <div style={listStyle} className="offset-md-4 col-md-4">
-        <h4 align="center"> Nodes </h4>
-        <ListGroup>
-          {this.state.nodes.map((nodes, index) =>
-            <ListGroupItem key={index}> Name: {nodes.name} Location: {nodes.location} Created: {nodes.created}  </ListGroupItem>
-          )}
-        </ListGroup>
+      <div >
+        <Location  />
       </div>
     );
+  }
+
+}
+
+class Location extends App {
+  constructor(props){
+    super(props)
+  };
+
+  componentDidMount() {
+    this.fetchLocation();
+  }
+
+  fetchLocation(){
+    fetch('http://localhost:3000/api/locations')
+    .then(response => response.json())
+    .then(jsonData =>{
+      console.log(jsonData)
+      const locations = jsonData.locations.map(object => (object)) //this is an object with an array in it
+      this.setState({locations})
+    })
+  }
+
+  render(){
+    return( 
+      <div className="offset-md-4 col-md-4">
+        <h4 align="center"> Location </h4>
+        <ListGroup>
+          {this.state.locations.map((locations, index) =>
+            <ListGroupItem key={index}> {locations} </ListGroupItem>
+          )}
+        </ListGroup>
+    </div>);
   }
 
 }
